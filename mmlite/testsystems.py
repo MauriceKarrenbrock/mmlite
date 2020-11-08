367eec09c6f8f946638a5d5fa467c8746eef2767
simomarsili <simo.marsili@gmail.com>
2020-11-08 19:00:29 +0100

    rename to testsystems

--- mmlite/testsystems.py
+++ mmlite/testsystems.py
@@ -7,25 +7,6 @@ from simtk import openmm as mm
 from simtk import unit
 from simtk.openmm import app
 
-from .model import empty_positions, empty_topology
-
-
-def define_water_topology():
-    """Water molecule topology."""
-    topology = app.Topology()
-    # add `chain` to the topology
-    chain = topology.addChain()
-    # add a residue named "water" to the chain
-    residue = topology.addResidue('water', chain)
-    oxigen = app.Element.getByAtomicNumber(8)
-    hydrogen = app.Element.getByAtomicNumber(1)
-    atom0 = topology.addAtom('O', oxigen, residue)
-    atom1 = topology.addAtom('H', hydrogen, residue)
-    atom2 = topology.addAtom('H', hydrogen, residue)
-    topology.addBond(atom0, atom1)
-    topology.addBond(atom0, atom2)
-    return topology
-
 
 class Water(TestSystem):
     """Create a single tip3pfb water molecule."""
@@ -33,23 +14,13 @@ class Water(TestSystem):
 
         TestSystem.__init__(self, **kwargs)
 
-        model = 'tip3pfb'
-        # load the forcefield
+        # load the forcefield for tip3pfb
         ff = app.ForceField('amber14/tip3pfb.xml')
 
-        # Create new Modeller instance.
-        modeller = app.Modeller(empty_topology(), empty_positions())
-
-        # Add solvent
-        modeller.addSolvent(ff, model=model, numAdded=1)
-
-        self.topology = modeller.getTopology()
-
-        # Get new topology and coordinates.
-        pos = modeller.getPositions()
-
-        # Convert positions to numpy.
-        self.positions = unit.Quantity(np.array(pos / pos.unit), pos.unit)
+        self.topology = self.def_topology()
+        self.positions = unit.Quantity(
+            np.array([[0.0, 0.0, 0.0], [0.0, 0.0, 1.0], [0.0, 1.0, 0.0]]),
+            unit.angstroms)
 
         # Create OpenMM System.
         self.system = ff.createSystem(
@@ -57,7 +28,24 @@ class Water(TestSystem):
             nonbondedCutoff=mm.NonbondedForce.NoCutoff,
             constraints=None,
             rigidWater=False,
-            removeCMMotion=False)
+            removeCMMotion=True)
 
         n_atoms = self.system.getNumParticles()
         self.ndof = 3 * n_atoms
+
+    @staticmethod
+    def def_topology():
+        """Water molecule topology."""
+        topology = app.Topology()
+        # add `chain` to the topology
+        chain = topology.addChain()
+        # add a residue named "water" to the chain
+        residue = topology.addResidue('water', chain)
+        oxigen = app.Element.getByAtomicNumber(8)
+        hydrogen = app.Element.getByAtomicNumber(1)
+        atom0 = topology.addAtom('O', oxigen, residue)
+        atom1 = topology.addAtom('H', hydrogen, residue)
+        atom2 = topology.addAtom('H', hydrogen, residue)
+        topology.addBond(atom0, atom1)
+        topology.addBond(atom0, atom2)
+        return topology
