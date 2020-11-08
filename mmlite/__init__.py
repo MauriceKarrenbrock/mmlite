b0655af848b81e2072db3815f051b472ea11035f
simomarsili <simo.marsili@gmail.com>
2020-11-08 19:00:08 +0100

    rm view_traj

--- mmlite/__init__.py
+++ mmlite/__init__.py
@@ -4,4 +4,4 @@ from .output import add_reporters
 from .package import __title__, __version__
 from .utils import (minimize, set_simulation_temperature, simulation_energy,
                     simulation_forces, simulation_positions, simulation_state,
-                    simulation_velocities, view_traj, write_single_pdb)
+                    simulation_velocities, write_single_pdb)
