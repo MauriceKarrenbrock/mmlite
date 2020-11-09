0990558675abc60214c478542d625b5c75de977f
simomarsili <simo.marsili@gmail.com>
2020-11-09 17:33:13 +0100

    rename to plot_traj; add default representation

--- mmlite/show.py
+++ mmlite/show.py
@@ -7,13 +7,60 @@ import nglview
 import pandas
 from simtk import unit
 
+nglview_defaults = [
+    {
+        'type': 'cartoon',
+        'params': {
+            'sele': 'protein'
+        }
+        # }, {
+        # 'type': 'surface',
+        # 'params': {
+        # 'sele': 'protein',
+        # 'opacity': 0.1
+        # }
+    },
+    {
+        'type': 'ball+stick',
+        'params': {
+            'sele': 'hetero'
+        }
+    }
+]
 
-def pdb_traj(fp):
+
+def plot_traj(fp,
+              stride=None,
+              atom_indices=None,
+              ball_and_stick=False,
+              water=False):
     """Return a trajectory view."""
-    return nglview.show_mdtraj(mdtraj.load(fp))
+    traj = mdtraj.load(fp, stride=stride, atom_indices=atom_indices)
+    view = nglview.show_mdtraj(traj)
+    view.representations = nglview_defaults
+    if ball_and_stick:
+        try:
+            ball_and_stick = ball_and_stick.split()
+        except AttributeError:
+            pass
+        view.representations.extend([{
+            'type': 'ball+stick',
+            'params': {
+                'sele': x
+            }
+        } for x in ball_and_stick])
+    if water:
+        view.representations.append({
+            'type': 'ball+stick',
+            'params': {
+                'sele': 'water'
+            }
+        })
+    # print(view.representations)
+    return view
 
 
-def frame(xp, top):
+def plot_frame(xp, top):
     """Create an MDTraj Trajectory object and return a nglview view.
 
     Parameters
@@ -36,7 +83,7 @@ def frame(xp, top):
     return view
 
 
-def simulation_data(fp, x='time', y='energy', stride=None, rolling=None):
+def plot_simulation_data(fp, x='time', y='energy', stride=None, rolling=None):
     """Plot simulation data."""
     mapper = {
         'step': '#"Step"',
