fae9495e3fb682e7efd84d25b44a11f5ed879c3f
simomarsili <simo.marsili@gmail.com>
2020-11-20 17:36:30 +0100

    add MdSystem

--- mmlite/testsystems.py
+++ mmlite/testsystems.py
@@ -1,49 +1,126 @@
 # -*- coding: utf-8 -*-
 """Test systems."""
 # pylint: disable=unused-import, too-few-public-methods, no-member
-from numbers import Number
+import logging
+from abc import ABC  # python >= 3.4
 
+import mdtraj
+import nglview
 import numpy as np
 from openmmtools.testsystems import TestSystem
 from simtk import openmm as mm
 from simtk import unit
 from simtk.openmm import app
-from simtk.openmm.openmm import VerletIntegrator
+from simtk.openmm.openmm import LangevinIntegrator, VerletIntegrator
 
-from .simulation import set_simulation_positions, set_simulation_temperature
+from .simulation import (set_simulation_positions, set_simulation_temperature,
+                         simulation_energy)
+
+logger = logging.getLogger(__name__)
 
 
-class SimulationMixin:
-    """Helper methods for simulations."""
+class MdSystem(TestSystem, ABC):
+    """Base class for storing data for a simulation.
+
+    Parameters
+    ----------
+    integrator_class : Integrator object
+    dt : Quantity
+    friction : Quantity
+    temperature : Quantity
+    *args, **kwargs : to TestSystem
+
+    """
+    def __init__(self, *args, **kwargs):
+        self.integrator_class = kwargs.pop('integrator', VerletIntegrator)
+        self.dt = kwargs.pop('dt', 2.0 * unit.femtoseconds)
+        self.friction = kwargs.pop('friction', 91.0 / unit.picosecond)
+        self.temperature = kwargs.pop('temperature', 298.0 * unit.kelvin)
+        super().__init__(*args, **kwargs)
+
     @property
     def integrator(self):
-        """Default integrator."""
-        return VerletIntegrator
+        """Return a fresh integrator."""
+        if self.integrator_class.__name__ == 'VerletIntegrator':
+            return VerletIntegrator(self.dt)
+        if self.integrator_class.__name__ == 'LangevinIntegrator':
+            return LangevinIntegrator(self.temperature, self.friction, self.dt)
+        raise ValueError(self.integrator_class)
+
+    def langevin_integrator(self):
+        """Return a fresh Langevin integrator."""
+        return mm.LangevinIntegrator(self.temperature, self.friction, self.dt)
+
+    def verlet_integrator(self):
+        """Return a fresh Verlet integrator."""
+        return mm.VerletIntegrator(self.dt)
 
     @property
-    def temperature(self):
-        """Default temperature."""
-        return unit.Quantity(298, unit.kelvin)
-
-    def simulation(self, integrator=None, temperature=None):
+    def simulation(self):
         """Return a Simulation object."""
-        if integrator is None:
-            integrator = self.integrator(1 * unit.femtoseconds)
-        elif isinstance(integrator, Number):
-            integrator = self.integrator(integrator * unit.femtoseconds)
-        if temperature is None:
-            temperature = self.temperature
-        sim = app.Simulation(self.topology, self.system, integrator)
+        sim = app.Simulation(self.topology, self.system, self.integrator)
         set_simulation_positions(sim, self.positions)
-        set_simulation_temperature(sim, temperature=temperature)
+        set_simulation_temperature(sim, temperature=self.temperature)
         return sim
 
+    def context(self, xp=None, random_vp=True):
+        """Return a simulation context."""
+        context = mm.Context(self.system, self.integrator)
+        if xp:
+            context.setPositions(xp)
+        else:
+            context.setPositions(self.positions)
+        if random_vp:
+            context.setVelocitiesToTemperature(self.temperature)
+        return context
 
-class Water(SimulationMixin, TestSystem):
+    @property
+    def mdtraj(self):
+        """mdtraj object from actual positions."""
+        top = mdtraj.Topology.from_openmm(self.topology)
+        return mdtraj.Trajectory([self.positions / unit.nanometers], top)
+
+    @property
+    def view(self):
+        """Return a nglview view for the actual positions."""
+        view = nglview.show_mdtraj(self.mdtraj)
+        if len(self.positions) < 10000:
+            view.add_ball_and_stick('all')
+        view.center(zoom=True)
+        return view
+
+    def minimize(self, tol=10 * unit.kilojoule / unit.mole, max_iter=None):
+        """Perform a local energy minimization on the system.
+
+        Parameters
+        ----------
+        simulation : Simulation or Context object.
+        tol : energy=10*kilojoules/mole
+            The energy tolerance to which the system should be minimized
+        max_iter : int=None
+            The maximum number of iterations to perform.  If this is 0,
+            Default: minimization is continued until the results converge.
+
+        Returns
+        -------
+        Context or Simulation.
+
+        """
+
+        ctx = self.context()
+        max_iter = max_iter or 0
+        logger.info('Energy before minimization: %s',
+                    simulation_energy(ctx)['potential'])
+        mm.LocalEnergyMinimizer.minimize(ctx, tol, maxIterations=max_iter)
+        logger.info('Energy after minimization: %s',
+                    simulation_energy(ctx)['potential'])
+
+
+class Water(MdSystem):
     """Create a single tip3pfb water molecule."""
-    def __init__(self, **kwargs):
+    def __init__(self, *args, **kwargs):
 
-        TestSystem.__init__(self, **kwargs)
+        super().__init__(*args, **kwargs)
 
         # load the forcefield for tip3pfb
         ff = app.ForceField('amber14/tip3pfb.xml')
