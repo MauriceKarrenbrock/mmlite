7e6ac1f5bcc7934bb274853ab00541faf78b279f
simomarsili <simo.marsili@gmail.com>
2021-03-16 17:45:11 +0100

    bump version to v0.3

--- mmlite/package.py
+++ mmlite/package.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """openmm_lite package info"""
 __title__ = 'mmlite'
-__version__ = '0.2.11'
+__version__ = '0.3'
 __author__ = 'Simone Marsili'
 __summary__ = ''
 __url__ = 'https://github.com/simomarsili/mmlite'
