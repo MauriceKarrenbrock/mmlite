d7c19467f2bad0c232c9d67486e02cad3a5ee84d
MauriceKarrenbrock <56843667+MauriceKarrenbrock@users.noreply.github.com>
2021-06-30 16:40:22 +0200

    removed old topography.py and renamed matrix.py -> topography.py

--- mmlite/__init__.py
+++ mmlite/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """Template init file"""
-from mmlite.matrix import Topography
 from mmlite.output import add_reporters
 from mmlite.package import __title__, __version__
+from mmlite.topography import Topography
 
 SEED = 1234
